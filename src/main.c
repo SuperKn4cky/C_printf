a0185ccaa2977baa43b784f1c5b1892b0b4efd0d
Quentin <quentin.gimenez@ecole-89.com>
2022-12-01 16:56:03 +0100

    printf save

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 
 int main(void)
 {
-    //printf("%c\n", 'a');
-    return (stu_dprintf(1, "str: %s\ndigit: %d\nchar: %c\nadress: %p\n", "Zozo", 12345, 'a', stu_strdup("ab")));
+    printf("%-999d\n", 56);
+    return (stu_dprintf(1, "%: %%\nstr: %s\ndigit: %d\nchar: %c\nadress: %p\n",
+                        "monkey", 12345, 'a', stu_strdup("test")));
 }
