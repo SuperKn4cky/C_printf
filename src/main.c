c63c37406bc0a87809261e4c8bab0117bdb791cd
Quentin <quentin.gimenez@ecole-89.com>
2022-12-06 16:47:55 +0100

    printf save

--- src/main.c
+++ src/main.c
@@ -7,21 +7,21 @@ int main(void)
     char *str1 = "Monkey";
     char *str2 = NULL;
 
-    printf("%+9d\n", 569);
-
-    stu_dprintf(1, "stu_dprintf :\n%%      :  %%\n");
-    stu_dprintf(1, "char * :  %s\n", "Monkey");
-    stu_dprintf(1, "char   :  %c\n", 'a');
-    stu_dprintf(1, "int    :| %d\n        | %d\n", 123456789, -123456789);
-    stu_dprintf(1, "adress :| %p\n        | %p\n", str1, str2);
+    stu_dprintf(1, "stu_dprintf :\n%%        :  %%\n");
+    stu_dprintf(1, "bourrage :  %+9d\n", 569);
+    stu_dprintf(1, "char *   :  %s\n", "Monkey");
+    stu_dprintf(1, "char     :  %c\n", 'a');
+    stu_dprintf(1, "int      :| %d\n          | %d\n", 123456789, -123456789);
+    stu_dprintf(1, "adress   :| %p\n          | %p\n", str1, str2);
 
     printf("\n");
 
-    dprintf(2, "dprintf :\n%%      :  %%\n");
-    dprintf(2, "char * :  %s\n", "Monkey");
-    dprintf(2, "char   :  %c\n", 'a');
-    dprintf(1, "int    :| %d\n        | %d\n", 123456789, -123456789);
-    dprintf(2, "adress :| %p\n        | %p\n", str1, str2);
+    dprintf(2, "dprintf  :\n%%        :  %%\n");
+    dprintf(2, "bourrage :  %+9d\n", 569);
+    dprintf(2, "char *   :  %s\n", "Monkey");
+    dprintf(2, "char     :  %c\n", 'a');
+    dprintf(1, "int      :| %d\n          | %d\n", 123456789, -123456789);
+    dprintf(2, "adress   :| %p\n          | %p\n", str1, str2);
 
     /*
      * int n;
