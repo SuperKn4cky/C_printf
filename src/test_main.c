ef30a97c3cab820dad535dbe6bd70b392ef1cd98
Quentin <quentin.gimenez@ecole-89.com>
2022-12-16 15:52:08 +0100

    printf.a save

--- src/test_main.c
+++ src/test_main.c
@@ -3,18 +3,23 @@
 
 int main(void)
 {
-    char *str1 = "Monkey";
-    char *str2 = NULL;
 
-    stu_dprintf(1, "stu_dprintf :\n%%        :  %%\n");
-    stu_dprintf(1, "char *   :  %s\n", "Monkey");
-    stu_dprintf(1, "char     :  %c\n", 'a');
-    stu_dprintf(1, "int      :| %d\n          | %d\n", 123456789, -123456789);
-    stu_dprintf(1, "adress   :| %p\n          | %p\n", str1, str2);
-    stu_dprintf(1, "bourrage :| $%+9d$\n", -569);
-    stu_dprintf(1, "         :| $%-9d$\n", -569);
-    stu_dprintf(1, "         :| $%+9d$\n", 569);
-    stu_dprintf(1, "         :| $%-9d$\n", 569);
-    stu_dprintf(1, "         :| $%09d$\n", 569);
+    stu_dprintf(1, " %d\n", 123456789);
+
+    /*
+     * char *str1 = "Monkey";
+     * char *str2 = NULL;
+     * 
+     * stu_dprintf(1, "stu_dprintf :\n%%        :  %%\n");
+     * stu_dprintf(1, "char *   :  %s\n", "Monkey");
+     * stu_dprintf(1, "char     :  %c\n", 'a');
+     * stu_dprintf(1, "int         %d\n            %d\n", 123456789, -123456789);
+     * stu_dprintf(1, "adress   :  %p\n          | %p\n", str1, str2);
+     * stu_dprintf(1, "bourrage :  $%+9d$\n", -569);
+     * stu_dprintf(1, "         :  $%-9d$\n", -569);
+     * stu_dprintf(1, "         :  $%+9d$\n", 569);
+     * stu_dprintf(1, "         :  $%-9d$\n", 569);
+     * stu_dprintf(1, "         :  $%09d$\n", 569);
+     */
     return 0;
 }
